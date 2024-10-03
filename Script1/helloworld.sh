aeef1994330a7a2ef034db4ec82b791d54a22809
CarloRaulSolisSanchez <73565579+CarloRaulSolisSanchez@users.noreply.github.com>
2024-10-02 19:38:01 -0600

    Add files via upload

---
+++ Script1/helloworld.sh
@@ -0,0 +1,11 @@
+echo "Hello World"
+
+echo "Contenido del Fichero (1)"
+ls
+
+mkdir Test
+
+cd Test
+
+echo "Contenido del Fichero (2)"
+ls
---
+++ Script2/create.sh
@@ -0,0 +1,14 @@
+echo "Hola Mundo" > mytext.txt
+
+cat mytext.txt
+
+mkdir backup
+
+mv mytext.txt backup/
+
+echo "Backup contenido"
+ls backup
+
+rm backup/mytext.txt
+
+rmdir backup
